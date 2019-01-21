4d47b0cf6b30e6da0c439328cfa18a85bcb24da5
Alain Abboud <abboudalain@gmail.com>
2019-01-21 13:15:55 -0500

    Voici l'esti de gros projet

---
+++ PetitesPuces_Q/PetitesPuces_Q/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetitesPuces_Q.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
