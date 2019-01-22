b60f558c5e06a4c0d4d9f2cb8be2d3794db4f958
Antoine Brassard <antoine.brassard@gmail.com>
2019-01-22 16:23:52 -0500

    suppression des pages par défaut de MVC

--- PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
+++ PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
@@ -15,19 +15,6 @@ namespace PetitesPuces.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult Catalogue()
         {
             ViewBag.Message = "Catalogue";
