f7245798a382c69691b29632597e9dd5b1babef9
Vincent <von3007@live.ca>
2019-01-22 17:05:38 -0500

    restructuration des fichier client

---
+++ PetitesPuces_Q/PetitesPuces/Controllers/ClientController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetitesPuces.Models;
+
+namespace PetitesPuces.Controllers
+{
+    public class ClientController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Catalogue()
+        {
+            ViewBag.Message = "Catalogue";
+            
+            /**
+             * Créer des produits bidons
+             */
+            Random random = new Random();
+            var produits = new List<Product>();
+            for (int i = 1; i <= 20; i++)
+            {           
+                var next = random.NextDouble();
+
+                var prix = 5.00 + (next * (1000.00 - 5.00));
+                produits.Add(new Product(i, "Produit No."+i){Price = prix});
+            }
+
+            //Mettre la liste de produits dans le viewModel qui va ensuite être envoyé vers le view
+            var viewModel = new CatalogueViewModel
+            {
+                Produits = produits
+            };
+
+            return View(viewModel);
+        }
+        public ActionResult MonPanier()
+        {
+            ViewBag.Message = "Votre panier";
+
+            return View();
+        }
+        
+    }
+}
--- PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
+++ PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
@@ -13,39 +13,6 @@ namespace PetitesPuces.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult Catalogue()
-        {
-            ViewBag.Message = "Catalogue";
-            
-            /**
-             * Créer des produits bidons
-             */
-            Random random = new Random();
-            var produits = new List<Product>();
-            for (int i = 1; i <= 20; i++)
-            {           
-                var next = random.NextDouble();
-
-                var prix = 5.00 + (next * (1000.00 - 5.00));
-                produits.Add(new Product(i, "Produit No."+i){Price = prix});
-            }
-
-            //Mettre la liste de produits dans le viewModel qui va ensuite être envoyé vers le view
-            var viewModel = new CatalogueViewModel
-            {
-                Produits = produits
-            };
-
-            return View(viewModel);
-        }
-        public ActionResult MonPanier()
-        {
-            ViewBag.Message = "Votre panier";
-
-            return View();
-        }
-        
+        }        
     }
 }
