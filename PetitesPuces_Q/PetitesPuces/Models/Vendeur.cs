0dcdc8bcad45fc2757889326fde4223ca71c6362
Antoine Brassard <antoine.brassard@gmail.com>
2019-01-22 21:09:03 -0500

    créatiion d'un dossier pour les ViewModels

---
+++ PetitesPuces_Q/PetitesPuces/Models/Vendeur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class Vendeur
+    {
+        public int NoVendeur { get; set; }
+        public string NomAffaires { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+    }
+}
