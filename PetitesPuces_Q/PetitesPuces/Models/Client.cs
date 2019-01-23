01df423e3d550db1e19e0ba1ce140680710e8b8c
Antoine Brassard <antoine.brassard@gmail.com>
2019-01-23 10:51:47 -0500

    ch-ch-ch-changes

--- PetitesPuces_Q/PetitesPuces/Models/Client.cs
+++ PetitesPuces_Q/PetitesPuces/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace PetitesPuces.Models
 {
     public class Client
     {
+        [DisplayName("No Client")]
         public int NoClient { get; set; }
+
+        [DisplayName("Adresse Courriel")]
         public string AdresseEmail { get; set; }
+
+        [DisplayName("Nom")]
         public string Nom { get; set; }
+
+        [DisplayName("Prénom")]
         public string Prenom { get; set; }
+
+        [DisplayName("Date de dernière connexion")]
         public DateTime DateDerniereConnexion { get; set; }
     }
 }
