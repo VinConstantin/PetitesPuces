46d1d7901541faf3b777d048fbe3e84af0eef896
Simon686 <44792308+Simon686@users.noreply.github.com>
2019-01-23 18:38:49 -0500

    FinalSimon

--- PetitesPuces_Q/PetitesPuces/Controllers/ClientController.cs
+++ PetitesPuces_Q/PetitesPuces/Controllers/ClientController.cs
@@ -55,6 +55,15 @@ namespace PetitesPuces.Controllers
             
             return View();
         }
-        
+
+        public ActionResult Profil()
+        {
+            return View();
+        }
+        public ActionResult modificationMDP()
+        {
+            return View();
+        }
+
     }
 }
