f9d1f8d5629b02040bce32b5867b31b6d28737c1
Simon686 <44792308+Simon686@users.noreply.github.com>
2019-01-23 14:00:20 -0500

    connexion,profil,oubli,modiMDP

---
+++ PetitesPuces_Q/PetitesPuces/Controllers/OubliMDPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class OubliMDPController : Controller
+    {
+        // GET: OubliMDP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
