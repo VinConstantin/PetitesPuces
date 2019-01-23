e251b1f4933be3df872a7710eec92357e5009ada
Antoine Brassard <antoine.brassard@gmail.com>
2019-01-22 21:10:43 -0500

    Vues, Vues partielles

---
+++ PetitesPuces_Q/PetitesPuces/Views/InactiveViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class InactiviteViewModel
+    {
+        public List<Vendeur> VendeursInactifs { get; set; }
+        public List<Client> ClientsInactifs { get; set; }
+    }
+}
