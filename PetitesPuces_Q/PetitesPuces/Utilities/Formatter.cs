1db1d62ce1a0b77b499ea61a686856f399955d0e
Vincent <von3007@live.ca>
2019-01-23 11:11:27 -0500

    catalogue par vendeur

--- PetitesPuces_Q/PetitesPuces/Utilities/Formatter.cs
+++ PetitesPuces_Q/PetitesPuces/Utilities/Formatter.cs
@@ -7,8 +7,8 @@ namespace PetitesPuces.Utilities
     {
         public static HtmlString Money(double value)
         {
-            var str = "<strong>"+value.ToString("0")+"</strong>" +
-                         "<sup>"+value.ToString("1.00").Split('.')[1]+"</sup> " +
+            var str = "<strong>"+value.ToString("0.00").Split('.')[0]+"</strong>" +
+                         "<sup>"+value.ToString("0.00").Split('.')[1]+"</sup> " +
                          "<strong>$</strong>";
             
             HtmlString html = new HtmlString(str);
