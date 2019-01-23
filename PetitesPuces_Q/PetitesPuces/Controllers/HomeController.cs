223bec7f435b1e34788e4397a4f6dd7a3458274c
Simon686 <44792308+Simon686@users.noreply.github.com>
2019-01-23 10:53:42 -0500

    Ajout page inscription utilisateurs

--- PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
+++ PetitesPuces_Q/PetitesPuces/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PetitesPuces.Models;
 
-namespace PetitesPuces.Controllers
+namespace TestMVC.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+
             return View();
-        }        
+        }
     }
 }
