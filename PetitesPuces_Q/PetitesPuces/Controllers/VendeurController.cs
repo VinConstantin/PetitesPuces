55dc16c767983ea44dba379fdcfe7b22c6032760
Alain Abboud <abboudalain@gmail.com>
2019-01-23 11:12:06 -0500

    Commande partial view

--- PetitesPuces_Q/PetitesPuces/Controllers/VendeurController.cs
+++ PetitesPuces_Q/PetitesPuces/Controllers/VendeurController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PetitesPuces.Models;
+using PetitesPuces.ViewModels.VMVendeur;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,37 @@ namespace PetitesPuces.Controllers
         // GET: Vendeur
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new CommandesViewModel
+            {
+                Commandes = new List<Commande>
+                {
+                    new Commande
+                    {
+                        NoCommande = 1,
+                        NomClient = "Antoinette",
+                        DateCommande = new DateTime(2018, 12, 28),
+                        Type = "Express",
+                        TotalAvantTaxes = 99.00
+                    },
+                    new Commande
+                    {
+                        NoCommande = 2,
+                        NomClient = "Vincent",
+                        DateCommande = new DateTime(2019, 01, 05),
+                        Type = "Standard",
+                        TotalAvantTaxes = 49.99
+                    },
+                    new Commande
+                    {
+                        NoCommande = 3,
+                        NomClient = "Simon",
+                        DateCommande = new DateTime(2019, 01, 09),
+                        Type = "Express",
+                        TotalAvantTaxes = 199.99
+                    }
+                }
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ PetitesPuces_Q/PetitesPuces/Models/Commande.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class Commande
+    {
+        public int NoCommande { get; set; }
+        public string NomClient { get; set; }
+        public DateTime DateCommande { get; set; }
+        public string Type { get; set; }
+        public double TotalAvantTaxes { get; set; }
+    }
+}
