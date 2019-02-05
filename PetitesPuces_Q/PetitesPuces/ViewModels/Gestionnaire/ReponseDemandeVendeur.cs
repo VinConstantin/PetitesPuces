9835e1f347a34612261320f5bb231cbce8f2a967
Antoine Brassard <antoine.brassard@gmail.com>
2019-02-05 10:55:37 -0500

    Gestion des demandes *devrait* être fonctionnel

---
+++ PetitesPuces_Q/PetitesPuces/ViewModels/Gestionnaire/ReponseDemandeVendeur.cs
@@ -0,0 +1,8 @@
+namespace PetitesPuces.ViewModels.Gestionnaire
+{
+    public class ReponseDemandeVendeur
+    {
+        public bool Accepte { get; set; }
+        public string Message { get; set; }
+    }
+}
