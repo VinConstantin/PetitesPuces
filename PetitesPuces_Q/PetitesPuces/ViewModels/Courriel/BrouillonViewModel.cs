91479d33ceba3761035dd119e641f7ab48325f96
Antoine Brassard <antoine.brassard@gmail.com>
2019-02-14 15:37:16 -0500

    Email and drafts

--- PetitesPuces_Q/PetitesPuces/ViewModels/Courriel/BrouillonViewModel.cs
+++ PetitesPuces_Q/PetitesPuces/ViewModels/Courriel/BrouillonViewModel.cs
@@ -5,9 +5,8 @@ namespace PetitesPuces.ViewModels.Courriel
     public class BrouillonViewModel
     {
         public long? NoMsg { get; set; }
-        public int NoExpediteur { get; set; }
         public string DescMsg { get; set; }
-        public short Lieu { get; set; }
+        public short? Lieu { get; set; }
         public string objet { get; set; }
         public List<int> destinataires { get; set; }
     }
